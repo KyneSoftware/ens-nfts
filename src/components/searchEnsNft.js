30bdce2b6b4e7f20c2e61b0f803e2bcd2afa16c7
Oisín Kyne <kyneoisin@gmail.com>
2020-06-26 17:57:43 +0100

    Search refactored into component with stateful react hooks

---
+++ src/components/searchEnsNft.js
@@ -0,0 +1,114 @@
+/**
+ * Search ENS NFT component that queries connected Ethereum service for an existing NFT pointed to by this name
+ */
+
+import React, { useState } from "react"
+import { Grid, TextField, Button, makeStyles, Avatar, Typography } from "@material-ui/core"
+import ExploreIcon from '@material-ui/icons/Explore';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: 'rebeccapurple',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+
+
+export default function SearchEns() {
+  const [searchValue, setSearchValue] = useState('')
+  const [helperText, setHelperText] = useState('Search for an NFT on ENS.')
+  const [validEnsName, setValidEnsName] = useState(true)
+
+  const onChange = event => {
+    setSearchValue(event.target.value)
+    console.log('Searching for : ' + searchValue)
+    setHelperText('Not a valid ENS name')
+  }
+
+  const onSubmit = event => {
+    event.preventDefault();
+    console.log('Searching for ENS NFT: ' + searchValue)
+    setHelperText('Cool name')
+    setValidEnsName(false)
+  }
+
+  // function checkIfEnsExists() {
+  //   console.log('Checking if valid ENS namehash present in registrar: ' + JSON.stringify(this.state.searchedEnsName))
+  //   return !!this.state.searchedEnsName
+  // }
+
+  // function handleLookupHelperText(nameExists) {
+  //   console.log('Getting Helper text for this name: ' + JSON.stringify(nameExists))
+  //   return "Input Helper Text"
+  // }
+
+  // function handleLookupInputChange(event) {
+  //   const ens = event.target.value;
+  //   console.log('ENS search updated: ' + ens)
+  //   return true
+  // }
+
+  // function handleEnsNameChange(event) {
+  //   console.log('ENS input updated: ' + JSON.stringify(event.target.value))
+  //   event.target.helperText = "Unknown ENS name"
+  //   return "Unknown ENS Name"
+  // }
+
+
+  const classes = useStyles();
+  return (
+    <div>
+      <Avatar className={classes.avatar}>
+        <ExploreIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Search for a named NFT
+      </Typography>
+      <form className={classes.form} noValidate onSubmit={onSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              onChange={onChange}
+              helperText={helperText}
+              error={!validEnsName}
+              name="ensName"
+              variant="outlined"
+              required
+              fullWidth
+              id="ensName"
+              value={searchValue}
+              label="ENS Name"
+            // InputLabelProps={{
+            //   shrink: true,
+            // }}
+            />
+          </Grid>
+        </Grid>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
+          Lookup NFT
+      </Button>
+      </form>
+    </div>
+  )
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,13 @@ import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import FiberNewIcon from '@material-ui/icons/FiberNew';
-import ExploreIcon from '@material-ui/icons/Explore';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import SearchEns from "../components/searchEnsNft"
 
 
 const useStyles = makeStyles((theme) => ({
@@ -35,36 +35,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-/* Checks if this ENS name is valid by checking it's formatting, then putting it through name hash, then finally checking on chain whether the NFT exists in the 
-ENS registry, in future, if the Name isn't in the registry but it's parent is, and this user's account is the owner of the parent name, I could register a subdomain on the fly.
-*/
-const handleEnsNameChange = (event) => {
-  console.log('ENS input updated: ' + JSON.stringify(event.target.value))
-  event.target.helperText = "Unknown ENS name"
-  return "Unknown ENS Name"
-}
 
-/*
-Performs namehash on the ENS input, and checks if it's present in the ENS registrar
-*/
-const handleLookupInputChange = (event) => {
-  const ens = event.target.value;
-  console.log('ENS search updated: ' + ens)
-  
-  const nameExists = checkIfEnsExists(ens)  
-  console.log('Does this ENS name exist?: ' + nameExists.toString())
-  return "Unknown ENS Name"
-}
-
-const checkIfEnsExists = (namehash) => {
-  console.log('Checking if valid ENS namehash present in registrar: ' + JSON.stringify(namehash))
-  return false
-}
-
-const handleLookupHelperText = (nameExists) => {
-  console.log('Getting Helper text for this name: ' + JSON.stringify(nameExists))
-  return "Input Helper Text"
-}
 
 export default function IndexPage() {
   const classes = useStyles();
@@ -74,38 +45,7 @@ export default function IndexPage() {
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <ExploreIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Search for a named NFT
-          </Typography>
-          <form className={classes.form} noValidate>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  error={checkIfEnsExists}
-                  onChange={handleLookupInputChange}
-                  helperText={"handleLookupHelperText"}
-                  name="ensName"
-                  variant="outlined"
-                  required
-                  fullWidth
-                  id="ensName"
-                  label="ENS Name"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Lookup NFT
-          </Button>
-          </form>
+          <SearchEns />
           <Avatar className={classes.avatar}>
             <FiberNewIcon />
           </Avatar>
@@ -116,8 +56,8 @@ export default function IndexPage() {
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
-                  error={true}
-                  onChange={handleEnsNameChange}
+                  error={false}
+                  onChange={()=>null}
                   name="ensName"
                   variant="outlined"
                   required
